75eb50aedc40bc08852ea8063ffd0403adbd720a
khemiriahmed <khemiriahmed6@gmail.com>
2026-07-10 19:46:01 +0100

    Message-details

---
+++ src/app/message-details/message-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-message-details',
+  templateUrl: './message-details.component.html',
+  styleUrls: ['./message-details.component.scss']
+})
+export class MessageDetailsComponent {
+  @Input()
+  message: any = {};
+}
